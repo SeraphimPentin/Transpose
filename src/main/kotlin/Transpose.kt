15b60d5cc4012ebf313d1b4acac81ac00e5a0090
SeraphimPentin <70956362+SeraphimPentin@users.noreply.github.com>
2021-04-23 21:43:31 +0300

    "Ťraňspöce" (I believe this version will be the last one)

--- src/main/kotlin/Transpose.kt
+++ src/main/kotlin/Transpose.kt
@@ -1,56 +1,73 @@
+import java.io.BufferedWriter
 import java.io.File
 import java.io.IOException
 
 fun main(args: Array<String>) {
     TransposeLauncher.main(args)
+    val tl = TransposeLauncher()
+    tl.parserArgs(args)
+    val output = if (tl.out == null) System.out.bufferedWriter() else File(tl.out).bufferedWriter()
+    val input = if (tl.input == null) {
+        System.`in`.bufferedReader()
+    } else File(tl.out).bufferedReader()
+    transpose(tl.a, tl.t, tl.r, output, tl.input)  // "input/example.txt"
+    output.close()
+    input.close()
 }
 
-fun transpose(flagA: Int, flagT: Boolean, flagR: Boolean, outputFile: String?, inputFile: String?) {
-    if (inputFile != null && !File(inputFile).isFile && !File(inputFile).exists()) throw IOException()
-    val input: List<String?> = if (inputFile == null) listOf(readLine())
-    else File(inputFile).readLines().map { it.trim() }
-
-    val countLine = input.size                     // кол-во сторок, для массива
-    val countWords = findMaxLengthLine(input)      // макс. кол-во слов в 1 стр
-    val arrayWords: Array<Array<String?>> = Array(countWords) { Array(countLine) { null } }
-
-    for ((numLine, line) in input.withIndex()) {
-        val splitStr = line?.split(" ")
-        if (splitStr != null) {
-            for ((numWord, word) in splitStr.withIndex()) {
-                when {
-                    flagT && word.length > flagA -> arrayWords[numWord][numLine] = word.substring(0, flagA)
-                    else -> {
-                        if (flagR) arrayWords[numWord][numLine] = word.padStart(flagA, ' ')
-                        if (!flagR) arrayWords[numWord][numLine] = word.padEnd(flagA, ' ')
-                    }
-                }
-            }
-        }
-    }
-
-    if (outputFile == null) {
-        for (i in 0 until countWords) {
-            for (j in 0 until countLine) {
-                if (arrayWords[i][j] != null) print(arrayWords[i][j] + " ")
-                else for (k in 0..flagA) print(" ")
-            }
-            if (i != countWords - 1) println()
-        }
-    } else if (!File(outputFile).isFile && !File(outputFile).exists()) throw IOException() else {
-        val output = File(outputFile.toString()).bufferedWriter()
-        output.use {
-            for (i in 0 until countWords) {
-                for (j in 0 until countLine) {
-                    if (arrayWords[i][j] != null) {
-                        it.append(arrayWords[i][j]).append(" ")
-                    } else for (k in 0..flagA) it.append(" ")
-                }
-                if (i != countWords - 1) it.appendLine()
-            }
-        }
-    }
-}
+//fun transpose(flagA: Int, flagT: Boolean, flagR: Boolean, output: BufferedWriter, inputFile: String) {
+//    val input = if (!File(inputFile).exists() && !File(inputFile).isFile) {
+//        while (true) {
+//            readLine()
+//        }
+//    } else File(inputFile).readLines().map { it.trim() }
+//    if (inputFile != null && !File(inputFile).isFile && !File(inputFile).exists()) throw IOException()
+//    val input: List<String> = if (inputFile == null) listOf(readLine())
+//    else File(inputFile).readLines().map { it.trim() }
+//    val input = File(inputFile).readLines().map { it.trim() }
+//
+//    val countLine = input.size                     // кол-во сторок, для массива
+//    val countWords = findMaxLengthLine(input)      // макс. кол-во слов в 1 стр
+//    val arrayWords: Array<Array<String?>> = Array(countWords) { Array(countLine) { null } }
+//
+//    for ((numLine, line) in input.withIndex()) {
+//        val splitStr = line?.split(" ")
+//        if (splitStr != null) {
+//            for ((numWord, word) in splitStr.withIndex()) {
+//                when {
+//                    flagT && word.length > flagA -> arrayWords[numWord][numLine] = word.substring(0, flagA)
+//                    else -> {
+//                        if (flagR) arrayWords[numWord][numLine] = word.padStart(flagA, ' ')
+//                        if (!flagR) arrayWords[numWord][numLine] = word.padEnd(flagA, ' ')
+//                    }
+//                }
+//            }
+//        }
+//    }
+//    System.out.bufferedWriter()
+//    if (outputFile == null) {
+//        for (i in 0 until countWords) {
+//            for (j in 0 until countLine) {
+//                if (arrayWords[i][j] != null) print(arrayWords[i][j] + " ")
+//                else for (k in 0..flagA) print(" ")
+//            }
+//            if (i != countWords - 1) println()
+//        }
+//    } else if (!File(outputFile).isFile && !File(outputFile).exists()) throw IOException() else {
+//        val output = File(outputFile.toString())//.bufferedWriter()
+//   val out = System.out.bufferedWriter().use {
+//            for (i in 0 until countWords) {
+//                for (j in 0 until countLine) {
+//                    if (arrayWords[i][j] != null) {
+//                        it.append(arrayWords[i][j]).append(" ")
+//                    } else for (k in 0..flagA) it.append(" ")
+//                }
+//                if (i != countWords - 1) it.appendLine()
+//            }
+//        }
+//    if(File(outputFile).isFile && File(outputFile).exists()) File(outputFile)
+//    }
+//}
 
 private fun findMaxLengthLine(input: List<String?>): Int {
     var res = 0
@@ -62,4 +79,38 @@ private fun findMaxLengthLine(input: List<String?>): Int {
     }
     return res
 }
+
+fun transpose(flagA: Int, flagT: Boolean, flagR: Boolean, output: BufferedWriter, inputFile: String) {
+    val input = File(inputFile).readLines().map { it.trim() }
+
+    val countLine = input.size
+    val countWords = findMaxLengthLine(input)
+    val arrayWords: Array<Array<String?>> = Array(countWords) { Array(countLine) { null } }
+
+    for ((numLine, line) in input.withIndex()) {
+        val splitStr = line.split(" ")
+        for ((numWord, word) in splitStr.withIndex()) {
+            when {
+                flagT && word.length > flagA -> arrayWords[numWord][numLine] = word.substring(0, flagA)
+                else -> {
+                    if (flagR) arrayWords[numWord][numLine] = word.padStart(flagA, ' ')
+                    if (!flagR) arrayWords[numWord][numLine] = word.padEnd(flagA, ' ')
+                }
+            }
+        }
+    }
+
+    for (i in 0 until countWords) {
+        for (j in 0 until countLine) {
+            if (arrayWords[i][j] != null) {
+                output.append(arrayWords[i][j]).append(" ")
+            } else for (k in 0..flagA) output.append(" ")
+        }
+        if (i != countWords - 1) output.appendLine()
+    }
+}
+
+
+
+
 
--- src/main/kotlin/TransposeLauncher.java
+++ src/main/kotlin/TransposeLauncher.java
@@ -1,30 +1,31 @@
 
 import org.kohsuke.args4j.*;
+
 import java.io.IOException;
 
 public class TransposeLauncher {
 
     @Option(name = "-a", usage = "Each word takes up num characters")
-    private int a = 10;
+    int a = 10;
 
     @Option(name = "-t", usage = "Crop the word to the desired size")
-    private boolean t = false;
+    boolean t = false;
 
     @Option(name = "-r", usage = "Align the word to the right border")
-    private boolean r = false;
+    boolean r = false;
 
     @Option(name = "-o", metaVar = "OutputName", usage = "Output file name")
-    private String out = null;
+    String out = null;
 
     @Argument
-    private String input = null;
+    String input = null;
 
 
     public static void main(String[] args) throws IOException {
         new TransposeLauncher().parserArgs(args);
     }
 
-    private void parserArgs(String[] args) throws IOException {
+    void parserArgs(String[] args) throws IOException {
         CmdLineParser parser = new CmdLineParser(this);
 
         try {
@@ -37,6 +38,6 @@ public class TransposeLauncher {
             return;
         }
 
-        TransposeKt.transpose(a, t, r, out, input);
+//        TransposeKt.transpose(a, t, r, out, input);
     }
 }
